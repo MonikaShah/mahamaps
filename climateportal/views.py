220fb14f646094bc587ab56b694c1b21ef30ced7
monikashah <monikapatira@gmail.com>
2026-06-02 15:07:47 +0530

    Added custom link to iot dashbpard

--- climateportal/views.py
+++ climateportal/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
+from django.conf import settings
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {"iot_dashboard_url": settings.IOT_DASHBOARD_URL})
 @csrf_exempt
 def gwpz_view(request):
     return render(request, "gwpz.html")
+def ff_view(request):
+    return render(request, "forestfire.html")
